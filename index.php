446f70b2c9de69dc6be981ff7d40dd03a504bf8e
Clint Losee <clint.losee@gmail.com>
2015-03-08 14:52:40 -0600

    Added blog page styling. Added sidebar styling.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
     <div class="container">
-        <div id="primary" class="content-area col-sm-9">
+        <div id="primary" class="content-area col-md-8">
             <main id="main" class="site-main" role="main">
 
             <?php if ( have_posts() ) : ?>
--- sidebar.php
+++ sidebar.php
@@ -4,12 +4,33 @@
  *
  * @package Amar
  */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
 ?>
 
-<div id="secondary" class="widget-area col-sm-3" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<div id="secondary" class="widget-area col-sm-12 col-md-4" role="complementary">
+	<div class="well">
+		<?php do_action( 'before_sidebar' ); ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+
+			<aside id="search" class="widget widget_search">
+				<?php get_search_form(); ?>
+			</aside>
+
+			<aside id="archives" class="widget">
+				<h3 class="widget-title"><?php _e( 'Archives', 'sparkling' ); ?></h3>
+				<ul>
+					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+				</ul>
+			</aside>
+
+			<aside id="meta" class="widget">
+				<h3 class="widget-title"><?php _e( 'Meta', 'sparkling' ); ?></h3>
+				<ul>
+					<?php wp_register(); ?>
+					<li><?php wp_loginout(); ?></li>
+					<?php wp_meta(); ?>
+				</ul>
+			</aside>
+
+		<?php endif; // end sidebar widget area ?>
+	</div>
 </div><!-- #secondary -->
